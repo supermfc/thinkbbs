d352650cc946974ffb17ef147dc66e42ee643dfc
DingBing <jzwtdb@163.com>
2019-12-09 13:03:26 +0800

    '创建用户模型的数据表'

---
+++ database/migrations/20191209045642_create_table_user.php
@@ -0,0 +1,52 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class CreateTableUser extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    /*public function change()
+    {
+        必须注释掉，否则只会执行change，不会执行up
+    }*/
+
+    public function up()
+    {
+        $table = $this->table('user',array('engine'=>'InnoDB'));
+        $table->addColumn('name', 'string', array('limit' => 50, 'default' => '', 'null' => false, 'comment' => '用户名'))
+            ->addColumn('mobile', 'string', array('limit' => 11, 'default' => '', 'null' => false, 'comment' => '注册手机'))
+            ->addColumn('password', 'string', array('limit' => 60, 'default' => '', 'null' => false, 'comment' => '登录密码'))
+            ->addColumn('avatar', 'string', array('default' => '', 'null' => false, 'comment' => '用户头像'))
+            ->addColumn('introduction', 'string', array('default' => '', 'null' => false, 'comment' => '个人介绍'))
+            ->addColumn('create_time', 'integer', array('default' => 0, 'signed' => false, 'null' => true))
+            ->addColumn('update_time', 'integer', array('default' => 0, 'signed' => false, 'null' => true))
+            ->addIndex(['mobile'], ['unique' => true])
+            ->save();
+    }
+
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
