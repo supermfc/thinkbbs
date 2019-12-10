7bd2aecaf7f4deadd0a0883baa6f27dd861065d3
DingBing <jzwtdb@163.com>
2019-12-10 19:45:33 +0800

    't'

---
+++ app/index/controller/User.php
@@ -0,0 +1,94 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\index\controller;
+
+use think\Request;
+use app\common\model\User as UserModel;
+
+class User extends Base
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        $user = UserModel::find(intval($id));
+        if(empty($user)){
+            // 当查看的用户不存在时跳转到首页
+            $message = '对不起，你访问页面不存在。';
+            Session::flash('danger', $message);
+            return $this->redirect('/');
+        }
+        //trace($user->id);
+        return $this->fetch('read', ['user' => $user]);
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
