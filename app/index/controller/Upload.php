aa1f1d72125272564a58a16ff9c1561a6dfcf8aa
DingBing <jzwtdb@163.com>
2019-12-16 09:04:11 +0800

    '上传图片'

---
+++ app/index/controller/Upload.php
@@ -0,0 +1,111 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\index\controller;
+
+use think\Request;
+use app\common\model\Upload as UploadModel;
+
+class Upload extends Base
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        return $this->save_image();
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        return $this->save_image();
+    }
+
+
+    private function save_image()
+    {
+        $backcall = $this->request->param('backcall');
+
+        $width = $this->request->param('width',100);
+        $height = $this->request->param('height',100);
+
+        if ($this->request->isPost()) {
+            $file = $this->request->file('image');
+            $upload_info = UploadModel::saveImage($file);
+
+            $image = $upload_info['save_path'];
+        } else {
+            $image = $this->request->param('image');
+        }
+
+        return $this->fetch('create',[
+            'backcall' => $backcall,
+            'width'     => $width,
+            'height'    =>  $height,
+            'image'     =>  $image,
+        ]);
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
