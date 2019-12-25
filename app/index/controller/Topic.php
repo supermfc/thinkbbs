916672b33128166a416d4a01dde331416a6c2b0a
DingBing <jzwtdb@163.com>
2019-12-25 15:39:48 +0800

    '用户可以创建话题'

--- app/index/controller/Topic.php
+++ app/index/controller/Topic.php
@@ -4,10 +4,17 @@ declare (strict_types = 1);
 namespace app\index\controller;
 
 use think\Request;
+use think\facade\Session;
+use app\common\model\Category as CategoryModel;
+use app\common\exception\ValidateException;
 use app\common\model\Topic as TopicModel;
 
 class Topic extends Base
 {
+    protected $middleware = [
+        'auth' => ['except' => ['index']],
+    ];
+
     /**
      * 显示资源列表
      *
@@ -30,6 +37,10 @@ class Topic extends Base
     public function create()
     {
         //
+         return $this->fetch('form', [
+            'categories' => CategoryModel::select(),
+            'topic' => [],
+        ]);
     }
 
     /**
@@ -40,7 +51,25 @@ class Topic extends Base
      */
     public function save(Request $request)
     {
-        //
+        if (!$this->request->isPost() || !$this->request->isAjax()) {
+            $message = '对不起，你访问页面不存在。';
+            Session::flash('danger', $message);
+            return $this->redirect('[topic.create]');
+        }
+
+        $message = null;
+        try {
+            $data = $this->request->post();
+            $topic = TopicModel::createItem($data);
+        } catch (ValidateException $e) {
+            return $this->error($e->getMessage(), null, ['errors' => $e->getData()]);
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+
+        $message = '话题创建成功。';
+        Session::flash('success', $message);
+        return $this->success($message, '[topic.index]');
     }
 
     /**
